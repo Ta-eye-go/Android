944641921ffddc2b6bc6a81202619841efc07716
min9ju <k2496625@gmail.com>
2022-01-14 10:49:36 +0900

    로그인 화면 추가

---
+++ app/src/main/java/com/code_23/ta_eye_go/ui/login/Login_email.kt
@@ -0,0 +1,12 @@
+package com.code_23.ta_eye_go.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.code_23.ta_eye_go.R
+
+class Login_email : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_email)
+    }
+}
