56d5f7e5d39cdc8aaaaa2a64727e2d3b96599871
Youkyung Yang <gosjorb@gmail.com>
2022-01-24 02:25:04 +0900

    버스 탑승 전후 UI 및 위치 기반 관련 추가

--- app/src/main/java/com/code_23/ta_eye_go/ui/main/MainActivity.kt
+++ app/src/main/java/com/code_23/ta_eye_go/ui/main/MainActivity.kt
@@ -1,22 +1,31 @@
 package com.code_23.ta_eye_go.ui.main
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import com.code_23.ta_eye_go.R
 import com.code_23.ta_eye_go.ui.bookbus.ChatbotMain
 import com.code_23.ta_eye_go.ui.bookmark.BookmarkList
 import com.code_23.ta_eye_go.ui.login.LoginEmail
 import com.code_23.ta_eye_go.ui.settings.Settings
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        fetchLocation()
 
         // 예약 창 이동
         bookBusBtn.setOnClickListener {
@@ -35,9 +44,28 @@ class MainActivity : AppCompatActivity() {
         }
         // 현 위치 새로고침
         refreshBtn.setOnClickListener {
-
+            fetchLocation()
+            // getCurrentStation()
         }
     }
 
+    private fun fetchLocation(){
+        val task = fusedLocationProviderClient.lastLocation
+
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),101)
+            return
+        }
+        task.addOnSuccessListener {
+            if(it != null){
+                currentLocationText.text = "현재 정류장(임시로 좌표 표시)\n ${it.latitude}\n ${it.longitude}"
+            }
+        }
+    }
+
+    private fun getCurrentStation(){
+
+    }
 
 }
